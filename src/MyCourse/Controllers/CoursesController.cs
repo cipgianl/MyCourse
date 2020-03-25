267bc478763f56d4083c5c0b5185cf1b663242e3
Gianluca Cipolletta <cipollettagianluca@gmail.com>
2020-03-25 12:17:20 +0100

    Aggiunto ASP.NET Core MVC

---
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers
+{
+    public class CoursesController : Controller
+    {
+        
+    }
+}
---
+++ src/MyCourse/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() {
+            return Content("Sono la index della Home");
+        }
+    }
+}
