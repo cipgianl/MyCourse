e757c8d730ef029cd04dc6679302c41cdf69c69c
Gianluca Cipolletta <cipollettagianluca@gmail.com>
2020-03-25 12:48:44 +0100

    Aggiunta ASP.NET Core 2.2

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -15,6 +15,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,9 +27,9 @@ namespace MyCourse
             }
             app.UseStaticFiles();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            // app.UseMvcWithDefaultRoute()
+            app.UseMvc(routeBuilder => {
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
