bc5eb3743688620c4d4db661c2e15b94300e16fb
Gianluca Cipolletta <cipollettagianluca@gmail.com>
2020-03-25 12:50:02 +0100

    Fine Sezione06

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -4,6 +4,12 @@ namespace MyCourse.Controllers
 {
     public class CoursesController : Controller
     {
-        
+        public IActionResult Index() {
+            return Content("Sono Index, di Courses");
+        }
+
+        public IActionResult Detail(string id) {
+            return Content($"Sono Detail, è stato scelto il corso: {id}");
+        }        
     }
 }
--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MyCourse
@@ -13,11 +14,11 @@ namespace MyCourse
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+        public void ConfigureServices(IServiceCollection services) { 
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
